d26a9f4983f2f5e0fae1a4173e8e856ff96e5339
aseke7182 <askar_pvl_kz@mail.ru>
2019-06-07 17:41:29 +0600

    a

--- frontik/src/_app/footer/Footer.js
+++ frontik/src/_app/footer/Footer.js
@@ -7,9 +7,9 @@ export class Footer extends Component {
             <div className="footer">
             	<label className="contact">Contact us via:</label>
             <div className="contact-items">
-            	<label className="mail contact-item row-sm-4">Email: <a href="mailto:pressFtoStayFit@gmail.com">PressFtoStayFit</a></label>
-            	<label className="phone contact-item row-sm-4">Phone: <a>8-727-327-77-6</a></label>
-            	<label className="tgg contact-item row-sm-4">Telegram: <a href="http://t.me/timque">@PressFtoStayFit</a></label>
+            	<label className="mail contact-item row-sm-4">Email: <a href="http://aseke7182@gmail.com">Online Shop</a></label>
+            	<label className="phone contact-item row-sm-4">Phone: <p> 8-707-681-33-41 </p> </label>
+            	<label className="tgg contact-item row-sm-4">Telegram: <a href="http://t.me/aseke7182">@aseke7182</a></label>
             </div>
             </div>
         )
