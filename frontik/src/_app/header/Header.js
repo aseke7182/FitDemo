43584b93f00c4b7a7a33ccb616f836d7109915b8
Kizatov_Arman <kizatov.armashka.05@gmail.com>
2019-06-07 15:36:00 +0600

    ыыыы

--- frontik/src/_app/header/Header.js
+++ frontik/src/_app/header/Header.js
@@ -5,7 +5,26 @@ import './Header.css';
 export class Header extends Component {
     render() {
         return (
-
+            <header>
+            	<nav className="group_of_elements">
+            		<ul>
+            			<li className="header_elements"><NavLink to="">Main Menu</NavLink></li>
+            			<li className="header_elements"><NavLink to="/catalogs">Catalogs</NavLink></li>
+                        if ( !localStorage.getItem('token')) {
+                            <li className="header_elements"><NavLink to="/auth">Login</NavLink></li>
+                        }
+                        else if( localStorage.getItem('token') ){
+                            <li className="header_elements"><NavLink to="/auth">Logout</NavLink></li>
+                        }
+            		</ul>
+            	</nav>
+            </header>
+            <div>
+                <NavLink to="">Main Page</NavLink>
+                <NavLink to="/catalogs">Catalogs</NavLink>
+                <NavLink to="/auth">Log In</NavLink>
+                <NavLink to="/register">Register</NavLink>
+            </div>
         )
     }
 }
