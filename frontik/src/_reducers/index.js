da9b96c2251de3e4ff343c1cb80a8601f554a18e
aseke7182 <askar_pvl_kz@mail.ru>
2019-06-08 16:26:24 +0600

    a

--- frontik/src/_reducers/index.js
+++ frontik/src/_reducers/index.js
@@ -3,11 +3,13 @@ import { combineReducers } from 'redux';
 import catalogsReducer from './catalogs.reducer';
 import comicsyReducer from './comicsy.reducer';
 import commentsReducer from './comments.reducer';
+import authReducer from "./auth.reducer";
 
 const appReducer = combineReducers({
     catalog: catalogsReducer,
     comics: comicsyReducer,
     comments: commentsReducer,
+    auth: authReducer
 });
 
 export default appReducer;
