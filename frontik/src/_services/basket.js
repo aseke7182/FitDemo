ece0e23a6fdf183c840ea5934d6dce0a05ba6efc
Kizatov_Arman <kizatov.armashka.05@gmail.com>
2019-06-08 17:50:34 +0600

    azaza

---
+++ frontik/src/_services/basket.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+const http = axios.create({
+    baseURL: 'http://localhost:8000/api/',
+    headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Token ' + localStorage.getItem('token')
+    }
+});
+
+export function createbaskett(status, ma){
+    return http.post('basket/', {
+        status: status,
+        ma: ma
+    })
+}
